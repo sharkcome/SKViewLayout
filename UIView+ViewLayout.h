479c68ebae3f90aeca8c1d228a360e69ecd5a3f9
谷建军 <1072705847@qq.com>
2017-03-24 13:18:39 +0800

    添加文件

---
+++ UIView+ViewLayout.h
@@ -0,0 +1,15 @@
+//
+//  UIView+ViewLayout.h
+//  SKStudent
+//
+//  Created by sharkcome on 17/3/24.
+//  Copyright © 2017年 sharkcome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (ViewLayout)
+
+- (void)fill;
+
+@end
---
+++ UIView+ViewLayout.m
@@ -0,0 +1,17 @@
+//
+//  UIView+ViewLayout.m
+//  SKStudent
+//
+//  Created by sharkcome on 17/3/24.
+//  Copyright © 2017年 sharkcome. All rights reserved.
+//
+
+#import "UIView+ViewLayout.h"
+
+@implementation UIView (ViewLayout)
+
+- (void)fill {
+    NSLog(@"__cme");
+}
+
+@end
